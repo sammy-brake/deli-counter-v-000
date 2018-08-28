35eafc12d16e5c057395106d518f656b39a2c791
Samantha brake <sammy.brake@gmail.com>
2018-08-28 23:36:29 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,7 +2,7 @@
 def line(katz_deli)
 
   if katz_deli.size == 0 
-    return "The line is currently empty."
+    puts "The line is currently empty."
     
   else
     final_line = "The line is currently: "
