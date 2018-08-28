259b9290fd674ef707984f4371e3cdd42c995d26
Samantha brake <sammy.brake@gmail.com>
2018-08-28 23:35:03 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,9 +1,10 @@
 # Write your code here.
 def line(katz_deli)
-final_line = "The line is currently: "
+
   if katz_deli.size == 0 
     puts "The line is currently empty."
   else
+    final_line = "The line is currently: "
     katz_deli.each do |name|
       counter = 1
       final_line << "#{counter}. #{name} "
