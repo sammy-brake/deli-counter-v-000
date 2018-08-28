ed9f055c87c23630edc166bf77026169639257a6
Samantha brake <sammy.brake@gmail.com>
2018-08-28 23:33:39 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,6 +7,7 @@ final_line = "The line is currently: "
     katz_deli.each do |name|
       counter = 1
       final_line << "#{counter}. #{name} "
-      
+      counter += 1 
   end 
+  puts final_line
 end 
